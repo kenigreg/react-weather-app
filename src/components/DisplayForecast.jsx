f5b68929b9aafca26d66ad27a660c023257f4aba
Gregory Oghenah <kenny@gregorys-mbp.home>
2024-04-30 12:57:43 +0100

    cleaned up the codes for components

--- src/components/DisplayForecast.jsx
+++ src/components/DisplayForecast.jsx
@@ -6,7 +6,6 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
-//import { SingleInputDateRangeField } from '@mui/x-date-pickers-pro/SingleInputDateRangeField';
 import { Container } from '@mui/material';
 import "./DisplayForecast.css";
 
